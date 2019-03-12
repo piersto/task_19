ee525c3bb2b1a3c89e7c663cff4279ad52c3a42b
PStoiko <pstoiko@gmail.com>
2019-03-12 12:48:26 -0400

    Started test_task_19 + test passed

--- fixture/home.py
+++ fixture/home.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 
 
 class HomeHelper:
@@ -12,3 +13,22 @@ class HomeHelper:
         webdriver.get("http://localhost/litecart/en/")
         WebDriverWait(webdriver, 10).until(EC.title_is('Online Store | My Store'))
 
+    def add_item_to_the_basket(self):
+        webdriver = self.app.webdriver
+
+        webdriver.find_element_by_css_selector('div#box-most-popular .product').click()
+
+        quantity_start = webdriver.find_element_by_css_selector('span.quantity').text
+        # Find out if element present
+        if len(webdriver.find_elements_by_css_selector("[name='options[Size]']")) > 0:
+            # If element is present, select 'Small' from drop-down menu
+            Select(webdriver.find_element_by_name("options[Size]")).select_by_visible_text("Small")
+            webdriver.find_element_by_name('add_cart_product').click()
+        else:
+            # If not present Click on 'Add to cart' button
+            webdriver.find_element_by_name('add_cart_product').click()
+        wait = WebDriverWait(webdriver, 10)  # seconds
+        wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
+
+        # Go back to Home page
+        webdriver.find_element_by_css_selector("li a[href$='/litecart/en/']").click()
--- test/test_add_product.py
+++ test/test_add_product.py
@@ -1,3 +1,7 @@
 def test_add_product(app):
     app.home.open_home_page()
+    app.home.add_item_to_the_basket()
+    app.home.add_item_to_the_basket()
+    app.home.add_item_to_the_basket()
+
 
